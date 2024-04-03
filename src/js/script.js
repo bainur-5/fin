98aa58e550b7501fb1fde05a500cc82646f28669
bainur-5 <bainur05ab@gmail.com>
2024-04-03 19:00:13 +0600

    finish

--- src/js/script.js
+++ src/js/script.js
@@ -4,36 +4,16 @@ const $header = document.querySelector('.header');
 let lastScrollTop = 0;
 
 $btnMenu.addEventListener('click', () => {
-    $menu.classList.toggle('h-screen');
-    if ($menu.classList.contains('translate-x-0')) {
-        $menu.classList.remove('translate-x-0');
-        $menu.classList.add('translate-x-full');
+    console.log('2');
+    if ($menu.classList.contains('translate-y-0')) {
+        $menu.classList.remove('translate-y-0');
+        $menu.classList.add('-translate-y-full');
     } else {
-        $menu.classList.remove('translate-x-full');
-        $menu.classList.add('translate-x-0');
+        $menu.classList.remove('-translate-y-full');
+        $menu.classList.add('translate-y-0');
     }
 });
 
-window.addEventListener('scroll', async () => {
-    const currentScroll = window.scrollY || document.documentElement.scrollTop;
-    
-    if (currentScroll > lastScrollTop && currentScroll > $header.offsetHeight) {
-        // Прокрутка вниз
-        
-        $header.classList.add('-translate-y-full');
-        await new Promise(resolve => setTimeout(resolve, 300)); // Даем 300 мс для выполнения анимации
-        $header.classList.remove('fixed');
-        $header.classList.remove('translate-y-0');
-    } else if (currentScroll < lastScrollTop) {
-        // Прокрутка вверх
-        $header.classList.remove('relative');
-        $header.classList.remove('-translate-y-full');
-        await $header.classList.add('fixed');
-        await $header.classList.add('translate-y-0')
-    }
-
-    lastScrollTop = currentScroll;
-});
 
 document.addEventListener('DOMContentLoaded', function () {
     const swiper = new Swiper('.swiper-container', {
